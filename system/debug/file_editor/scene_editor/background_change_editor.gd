b986c922491572134444859cc159aab8b37396c7
Alex Roman Nuñez <eirexe123@gmail.com>
2019-03-27 19:02:29 +0100

    cleanup

--- system/debug/file_editor/scene_editor/background_change_editor.gd
+++ system/debug/file_editor/scene_editor/background_change_editor.gd
@@ -5,15 +5,16 @@ var background_preview := TextureRect.new()
 func _ready():
 	extra_buttons_container.add_child(background_selector)
 	editable_area.add_child(background_preview)
-	background_preview.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_COVERED
+
 	background_selector.connect("item_selected", self, "on_background_changed")
+
 	for background in GameManager.backgrounds:
 		background_selector.add_item(background)
 		if background == line.background:
 			background_selector.select(background_selector.get_item_count()-1)
 			on_background_changed(background_selector.get_item_count()-1)
-			
-
+	
+	background_preview.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_COVERED
 	background_preview.expand = true
 	background_preview.set_anchors_and_margins_preset(Control.PRESET_HCENTER_WIDE)
 	background_preview.rect_size = Vector2(0,100)
